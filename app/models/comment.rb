f62b05586584dfe1528c0cb200b4e378662b1547
QED0711 <qdizon@gmail.com>
2019-03-03 18:26:18 -0500

    added Poject mothod to create new comment and link it to the user

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-
+    belongs_to :project
     belongs_to :user
 
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -3,5 +3,10 @@ class Project < ApplicationRecord
     belongs_to :user
     has_many :comments
 
+    def create_comment(text)
+        @user_id = self.user_id
+        self.comments.create({user_id: @user_id, text: text})
+    end
+
 
 end
